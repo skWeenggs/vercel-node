f9d5250feebbd6f5bff80fb0b161ea19e3701641
skWeenggs <104074453+skWeenggs@users.noreply.github.com>
2023-03-31 14:31:59 +0530

    add one more route-endpoint

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port =9000;
 app.use('/',(req,res)=>{
     res.json({massage:"hiiii"})
 })
+app.use('/call',(req,res)=>{
+    res.json({massage:"call massage"})
+})
 
 app.listen(900,()=>{
     console.log(`starting server on ${port}`);
