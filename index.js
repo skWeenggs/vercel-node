3da18eaed5400e7a3abc2ccea42822b580e5a9f4
weenggs-satish <satish.kumar.weenggs@gmail.com>
2023-03-31 16:52:08 +0530

    install dotenv package to read .env file.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express =require('express');
 const cors =require('cors');
 const {Client}=require('@notionhq/client')
+const dotenv=require('dotenv');
+dotenv.config();
 
 const app=express();
 app.use(cors());
-
+app.use(express.json())
 const port=5000;
 
 const notion= new Client({ auth:process.env.NOTION_KEY});
@@ -13,6 +15,22 @@ const notion= new Client({ auth:process.env.NOTION_KEY});
 app.get('/fetchpage1/:id', async(req,res)=>{
     try{
         console.log("id",req.params.id);
+        const users=await notion.databases.retrieve({
+            database_id:req.params.id
+
+        });
+        res.status(200).json({users})
+        console.log(users);
+        // return users.json();
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+app.get('/fetchuserdata/:id', async(req,res)=>{
+    try{
+        console.log("id",req.params);
         const users=await notion.databases.query({
             database_id:req.params.id
 
