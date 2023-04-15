f8fdcd44ed37aa519e9a85d96ec372487fab51d1
weenggs-satish <satish.kumar.weenggs@gmail.com>
2023-04-15 13:46:11 +0530

    add authors-page-id fields

--- index.js
+++ index.js
@@ -253,6 +253,7 @@ app.post('/submitFormToNotion', async(req,res)=>{
     const domain_name = req.body.domain;
     const contentPageId = req.body.content_page_id;
     const pagesPageId = req.body.pages_page_id;
+    const authorPageId = req.body.author_page_id;
     const token_secretid = req.body.token_secretid;
     const template= req.body.temp;
     
@@ -294,6 +295,14 @@ app.post('/submitFormToNotion', async(req,res)=>{
                 rich_text:[{
                     text:{
                         content: pagesPageId
+                    }
+                }
+                ]
+            },
+            AuthorPageId:{
+                rich_text:[{
+                    text:{
+                        content: authorPageId
                     }
                 }
                 ]
